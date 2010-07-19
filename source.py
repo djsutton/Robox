273f80cdded0fa6d959e702cc06343fb31f31d12
DJ Sutton <dj@laptop>
2010-07-19 12:16:08 +0000

    added a small sample source.py that will automatically load into the code area and serve as a simple demonstration of the capabilities of the Robot class so far

---
+++ source.py
@@ -0,0 +1,14 @@
+import time
+
+def goRobot(r):
+    while True:
+        if r.x > 50:
+            r.x=-50
+        if r.y > 50:
+            r.y=-50
+            
+        r.x += 1
+        r.y += 2
+        r.heading += 3
+        r.queueRedraw()
+        time.sleep(1/30.0)
